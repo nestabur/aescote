f1c1370c9f7c919874a9e8d6157a74152b3cc06d
gema <gema.santiago@canoo.com>
2015-10-03 03:40:35 +0200

    integrated firebase and example of three way data binding

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,29 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+// a factory to create a re-usable Profile object
+// we pass in a username and get back their synchronized data as an object
+.factory("Profile", ["$firebaseObject",
+  function($firebaseObject) {
+    return function(username) {
+      // create a reference to the database where we will store our data
+      //var randomRoomId = Math.round(Math.random() * 100000000);
+      //var ref = new Firebase("https://f2c2gl3tdy5.firebaseio-demo.com/data/" + randomRoomId);
+      var ref = new Firebase("https://f2c2gl3tdy5.firebaseio-demo.com/data");
+      var profileRef = ref.child(username);
+
+      // return it as a synchronized object
+      return $firebaseObject(profileRef);
+    }
+  }
+])
+
+.controller("ProfileCtrl", ["$scope", "Profile",
+  function($scope, Profile) {
+    // create a three-way binding to our Profile as $scope.profile
+    Profile("physicsmarie").$bindTo($scope, "profile");
+  }
+]);
+
+
